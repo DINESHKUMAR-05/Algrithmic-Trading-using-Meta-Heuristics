41fa18644fbd01cb36290c7a6ce623e99bb4e449
DINESHKUMAR-05 <111517362+DINESHKUMAR-05@users.noreply.github.com>
2024-03-05 18:54:19 +0530

    Update pso_algorithm.py

--- src/pso_algorithm.py
+++ src/pso_algorithm.py
@@ -19,28 +19,25 @@ class PSOConfig:
     max_velocity: float = 0.1
 
 class Individual:
-    def __init__(self, input_size, output_size):
-        self.name = '#' + ''.join(map(str, np.random.randint(0, 9, size=7).tolist()))
+    def __init__(self):
         self.num_epochs = np.random.choice(np.arange(60, 300))
         self.hidden_size = np.random.choice([2 ** power for power in range(2, 10)])
         self.num_layers = np.random.choice(np.arange(2, 15))
         self.learning_rate = round(np.random.random(), 2)
-        self.input_size = input_size
-        self.output_size = output_size
         self.loss = np.inf
         self.position = np.array([self.hidden_size, self.num_layers, self.learning_rate, self.num_epochs])
         self.velocity = np.zeros_like(self.position)
         self.best_position = np.copy(self.position)  # Initialize best_position attribute
 
     def __repr__(self):
-        return (f"Chromosome {self.name} with loss: {self.loss:.4}, "
+        return (f"Particle with loss: {self.loss:.4}, "
                 f"hidden_size: {self.hidden_size}, num_layers: {self.num_layers}, "
                 f"learning_rate: {self.learning_rate}, num_epochs: {self.num_epochs}")
 
 @dataclass
 class PopulationPSO:
-    def __init__(self, config: PSOConfig, input_size, output_size):
-        self.individuals = [Individual(input_size, output_size) for _ in range(config.num_particles)]
+    def __init__(self, config: PSOConfig):
+        self.individuals = [Individual() for _ in range(config.num_particles)]
         self.best_individual = min(self.individuals, key=lambda x: x.loss).position
 
 class PSO:
@@ -68,18 +65,18 @@ class PSO:
 
     def evaluate(self, X_val, y_val):
         for individual in self.population.individuals:
-            model = self.create_model(individual)
+            model = self.create_model(individual,X_val,y_val)
             optimizer = optim.Adam(model.parameters(), lr=individual.learning_rate)
             train(model, self.criterion, optimizer, device, X_val, y_val, individual.num_epochs, verbose=False, return_loss_history=False, compute_test_loss=False)
             individual.loss = predict(model, X_val, y_val, self.criterion, device)
             del model
 
-    def create_model(self, individual):
+    def create_model(self, individual, X_val, y_val):
         seed_everything(self.seed)
         if self.optimized_block == 'LSTM':
-            model = LSTM(input_size=individual.input_size, hidden_size=int(individual.hidden_size), num_layers=individual.num_layers).to(self.device)
+            model = LSTM(input_size=X_val.shape[2], hidden_size=int(individual.hidden_size), num_layers=individual.num_layers).to(self.device)
         elif self.optimized_block == 'GRU':
-            model = GRU(input_size=individual.input_size, hidden_size=int(individual.hidden_size), num_layers=individual.num_layers).to(self.device)
+            model = GRU(input_size=X_val.shape[2], hidden_size=int(individual.hidden_size), num_layers=individual.num_layers).to(self.device)
         else:
             raise ValueError('Only LSTM and GRU blocks are available for optimization.')
         return model
